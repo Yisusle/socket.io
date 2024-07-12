e3a46b496d081081cfd9f9b87dde3d89f9033a28
Jesus Leyva <jesusle116@gmail.com>
2024-07-11 18:24:16 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var cors = require('cors');
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+app.use(cors());
+
+app.use(express.static("client"));
+
+app.get("/hola-mundo", function(req, res){
+    res.status(200).send("Hola mundo desde una ruta");
+});
+
+var messages = [{
+    id:1,
+    text: "Bienvenido al chat de Jesus Leyva.",
+    nickname: "Bot - JesusLeyva.com"
+}];
+
+io.on("connection", function(socket){
+    console.log("Alguien se conectó. " + socket.handshake.address);
+    socket.emit("messages", messages);
+
+    socket.on("add-message", function(data){
+        messages.push(data);
+        io.sockets.emit("messages", messages);
+    });
+
+});
+
+//6677
+server.listen(3700, function(){
+    console.log("Servidor Funcionando.");
+});
