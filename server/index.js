a34273da2802660a42436d8658a412440409f9de
Jesus Antonio Leyva <jesusle116@gmail.com>
2026-06-17 12:29:11 -0600

    Implement secret message handling in chat server
    Added secret message functionality and improved structure.

--- server/index.js
+++ server/index.js
@@ -1,37 +1,59 @@
-var express = require("express");
-var app = express();
-var cors = require('cors');
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
+  var express = require("express");
+  var app = express();
+  var cors = require('cors');
+  var server = require("http").Server(app);
+  var io = require("socket.io")(server);
+  var crypto = require("crypto");
 
-app.use(cors());
+  app.use(cors());
+  app.use('/chat', express.static("client"));
+  app.use(express.static("client"));
 
-app.use('/chat', express.static("client"));
+  app.get("/hola-mundo", function(req, res){
+      res.status(200).send("Hola mundo desde una ruta");
+  });
 
-app.use(express.static("client"));
+  var messages = [{
+      id: 1,
+      text: "Bienvenido al chat de Jesus Leyva.",
+      nickname: "Bot - JesusLeyva.com"
+  }];
 
-app.get("/hola-mundo", function(req, res){
-    res.status(200).send("Hola mundo desde una ruta");
-});
+  var SECRET_HASH = crypto.createHash('sha256')
+      .update(process.env.SECRET_PASSWORD || "secreto")
+      .digest('hex');
 
-var messages = [{
-    id:1,
-    text: "Bienvenido al chat de Jesus Leyva.",
-    nickname: "Bot - JesusLeyva.com"
-}];
+  var secretMessages = [];
 
-io.on("connection", function(socket){
-    console.log("Alguien se conectó. " + socket.handshake.address);
-    socket.emit("messages", messages);
+  setInterval(function() {
+      secretMessages = [];
+      io.to("secret-room").emit("secret-messages", []);
+  }, 60 * 60 * 1000);
 
-    socket.on("add-message", function(data){
-        messages.push(data);
-        io.sockets.emit("messages", messages);
-    });
+  io.on("connection", function(socket){
+      console.log("Alguien se conectó. " + socket.handshake.address);
+      socket.emit("messages", messages);
 
-});
+      socket.on("add-message", function(data){
+          messages.push(data);
+          io.sockets.emit("messages", messages);
+      });
 
-//6677
-server.listen(process.env.PORT || 3700, function(){
-    console.log("Servidor Funcionando.");
-});
+      socket.on("join-secret", function(passwordHash){
+          if (passwordHash === SECRET_HASH) {
+              socket.join("secret-room");
+              socket.emit("secret-joined", secretMessages);
+          } else {
+              socket.emit("secret-denied");
+          }
+      });
+
+      socket.on("add-secret-message", function(data){
+          secretMessages.push(data);
+          io.to("secret-room").emit("secret-messages", secretMessages);
+      });
+  });
+
+  server.listen(process.env.PORT || 3700, function(){
+      console.log("Servidor Funcionando.");
+  });
